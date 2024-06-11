982c7763f50d91ce5c4db6f44bee286507099505
krls2020 <mail@karelhemza.cz>
2024-06-11 11:40:08 +0200

    chore

--- index.php
+++ index.php
@@ -6,24 +6,20 @@ use Ramsey\Uuid\Uuid;
 
 header('Content-Type: application/json');
 
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Handle health check
+if ($path == '/status') {
+    echo json_encode(['status' => 'UP']);
+    exit;
+}
+
+
 if (file_exists(__DIR__ . '/.env')) {
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 }
 
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-if ($path == '/status') {
-    echo json_encode(['status' => 'UP']);
-    exit;
-} elseif ($path != '/') {
-    // If the request is not to the root path, do not process further.
-    http_response_code(404); // Optionally, you can set this to 404 or another appropriate code.
-    echo "Not Found";
-    exit;
-}
-
-// Database operations only occur if the request is to the root path "/"
 $dbconn = pg_connect(
   "host=" . (getenv('DB_HOST') ?: 'localhost') . " " .
   "port=" . (getenv('DB_PORT') ?: '5432') . " " .
